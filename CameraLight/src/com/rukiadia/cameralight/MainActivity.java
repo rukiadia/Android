beaf50ac7c2476d81d65737f61e0a0efe4510296
rukiadia <secret.sign0401@gmail.com>
2013-02-19 16:23:13 +0900

    ・ライトがスリープ時に消灯しないように修正。 ・ライト点灯時はアプリ画面が消灯しないようにした。

--- CameraLight/src/com/rukiadia/cameralight/MainActivity.java
+++ CameraLight/src/com/rukiadia/cameralight/MainActivity.java
@@ -1,9 +1,15 @@
+/*
+ * スイッチでON/OFFを切り替える(だけ)懐中電灯アプリケーション
+ */
 package com.rukiadia.cameralight;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.WindowManager;
 import android.widget.CompoundButton;
+import android.widget.Toast;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.ToggleButton;
 import android.hardware.Camera;
@@ -18,33 +24,31 @@ public class MainActivity extends Activity implements OnCheckedChangeListener{
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.main);
 		
 		tb1 = (ToggleButton)findViewById(R.id.toggleButton1);
 		tb1.setOnCheckedChangeListener(this);
-		
+		//バックライトの自動消灯をON
+		getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 	}
 	
 	@Override
+	public void onResume(){
+		super.onResume();
+	}
+	
+	@Override	
 	public void onPause(){
 		super.onPause();
-		finish();
+		//スリープ時は消灯しない
+		//camera.release();
 	}
-	
+		
 	@Override
-	public void onStop(){
-		super.onStop();
-		//制御しているカメラデバイスのインスタンス
-		//アプリが終了した時も消灯
+	public void onDestroy(){
+		super.onDestroy();
 		camera.release();
 	}
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
-	}
-	
 
 	@Override
 	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -57,11 +61,27 @@ public class MainActivity extends Activity implements OnCheckedChangeListener{
 				cp.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
 				camera.setParameters(cp);
 				camera.startPreview();
+				//自動消灯をOFFに
+				getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 			} else if(isChecked == false) {
 				//ライトの消灯
 				camera.release();
+				//自動消灯をONに
+				getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 			}
 		}
 	}
-
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.menu, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item){
+		Toast.makeText(this, "Option", Toast.LENGTH_SHORT).show();
+		return super.onOptionsItemSelected(item);
+		
+	}
 }
