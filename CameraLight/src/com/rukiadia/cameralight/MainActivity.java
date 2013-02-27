7a266baa1e23266db4190d85e3d0042aceaeef89
rukiadia <secret.sign0401@gmail.com>
2013-02-27 18:06:55 +0900

    ノーティフィケーション通知機能追加・画面レイアウト変更・フォント変更追加

--- CameraLight/src/com/rukiadia/cameralight/MainActivity.java
+++ CameraLight/src/com/rukiadia/cameralight/MainActivity.java
@@ -5,6 +5,12 @@ package com.rukiadia.cameralight;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.WindowManager;
@@ -16,10 +22,16 @@ import android.hardware.Camera;
 
 public class MainActivity extends Activity implements OnCheckedChangeListener{
 	
-	ToggleButton tb1;
-	Camera camera;
-	Camera.Parameters cp;
-	CompoundButton buttonView;
+	private String ApplicationName = "CameraLight";
+	
+	//ノーティフィケーション情報
+	private static final int NotificationID = 100;
+	private NotificationManager manager;
+	
+	//トグルボタン、カメラオブジェクト
+	private ToggleButton tb1;
+	private Camera camera;
+	private Camera.Parameters cp;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -35,44 +47,91 @@ public class MainActivity extends Activity implements OnCheckedChangeListener{
 	@Override
 	public void onResume(){
 		super.onResume();
+		Log.d(ApplicationName, "onResume");
 	}
 	
 	@Override	
 	public void onPause(){
 		super.onPause();
-		//スリープ時は消灯しない
+		Log.d(ApplicationName, "onPause");
+		//スリープ時もライトを消さない
 		//camera.release();
 	}
 		
 	@Override
 	public void onDestroy(){
 		super.onDestroy();
+		Log.d(ApplicationName, "onDestroy");
 		//カメラオブジェクトを掴んでいない時は処理を行わない
 		if (camera != null){
 			camera.release();
 		}
+		//アプリ終了時にノーティフィケーションも消去
+		if (manager != null){
+			manager.cancel(NotificationID);
+		}
 	}
 
+	/* トグルボタンの処理 */
 	@Override
 	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-		//ToggleButtonの処理
 		if (buttonView.getId() == R.id.toggleButton1) {
 			if(isChecked == true) {
-				//ライトの点灯
+				//ライトの点灯処理
 				camera = Camera.open();
 				cp = camera.getParameters();
 				cp.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
 				camera.setParameters(cp);
 				camera.startPreview();
-				//自動消灯をOFFに
+				//端末の画面が消灯しないようにする
+				Log.d(ApplicationName, "ScreenOut ON");
 				getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+				//ノーティフィケーション表示
+				showNotification();
 			} else if(isChecked == false) {
 				//ライトの消灯
 				camera.release();
-				//自動消灯をONに
+				//端末の画面が消灯するようにする
+				Log.d(ApplicationName, "ScreenOut OFF");
 				getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+				
+				//ライトを消したら、ノーティフィケーションも消去
+				if (manager != null){
+					manager.cancel(NotificationID);
+				}
 			}
 		}
+	}
+	
+	/* ノーティフィケーション処理 */
+	public void showNotification(){
+		
+		Notification.Builder builder = new Notification.Builder(this);
+		//アイコン
+		builder.setSmallIcon(R.drawable.lightning);
+		//ステータスバーに表示されるタイトル
+		builder.setTicker("light up!");
+		//ノーティフィケーションが発行されるタイミング
+		builder.setWhen(System.currentTimeMillis());
+		
+		//ステータスバーを引き出した時に表示されるタイトル
+		builder.setContentTitle(ApplicationName);
+		//〜に表示される内容
+		builder.setContentText("the camera light lit up");
+		
+		//ノーティフィケーションをタップした時の処理インテント
+		Intent notificationIntent = new Intent(this, MainActivity.class);
+		//上記のインテントをアクティビティとして起動するためのPendingIntent
+		PendingIntent pIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+		//タップで起動するインテントをセット
+		builder.setContentIntent(pIntent);
+		
+		Notification notification = builder.getNotification();
+		
+		//発行
+		//NotificationManager manager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+		manager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+		manager.notify(NotificationID, notification);
 	}
 	
 	@Override
